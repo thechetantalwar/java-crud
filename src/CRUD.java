864ce7ac377a5c4ffb3cddda89ca50b952a346a5
Chetan Talwar <chetan.talwar@gmail.com>
2020-09-21 08:02:49 +0000

    Comment Updated

--- src/CRUD.java
+++ src/CRUD.java
@@ -4,7 +4,8 @@ public class CRUD {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost/doctor";
 
-    //  Database credentials
+    // Defining Database credentials
+   
     static final String USER = "root";
     static final String PASS = "12345678";
 
